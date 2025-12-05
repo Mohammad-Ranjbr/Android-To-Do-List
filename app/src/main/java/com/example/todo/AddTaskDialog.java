374bb71598ad94e0982592a7ad64ceeefbfd43df
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2025-12-05 13:47:23 +0330

    implement add task dialog ui

---
+++ app/src/main/java/com/example/todo/AddTaskDialog.java
@@ -0,0 +1,27 @@
+package com.example.todo;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class AddTaskDialog extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_add_task, null, false);
+        builder.setView(view);
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        return dialog;
+    }
+
+}
--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        View addNewTaskButton = findViewById(R.id.fab_main_addNewTask);
+        addNewTaskButton.setOnClickListener(v -> {
+            AddTaskDialog addTaskDialog = new AddTaskDialog();
+            addTaskDialog.show(getSupportFragmentManager(), null);
+        });
+
     }
 }
