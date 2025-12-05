349fdc9a202197f25201d65220845ae1c0ac9cbc
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2025-12-05 13:53:30 +0330

    implement edit task dialog ui

---
+++ app/src/main/java/com/example/todo/EditTaskDialog.java
@@ -0,0 +1,25 @@
+package com.example.todo;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class EditTaskDialog extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_edit_task, null, false);
+        builder.setView(view);
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        return dialog;
+    }
+}
