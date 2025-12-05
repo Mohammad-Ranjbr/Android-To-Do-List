4077c81e4d3d7a32dcca7985039f2ef47535d257
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2025-12-05 14:23:21 +0330

    create task class

---
+++ app/src/main/java/com/example/todo/Task.java
@@ -0,0 +1,33 @@
+package com.example.todo;
+
+public class Task {
+
+    private long id;
+    private String title;
+    private boolean isCompleted;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+}
